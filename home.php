ebcd3af3902bbf54ddf9bb600984b1a2e3f27d2b
Petani Kode <nurul adilah>
2022-02-19 16:09:11 +0700

    tugas

---
+++ home.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="styleta.css">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Project Tugas Akhir</title>
+  </head>
+  <body>
+    <style>
+        body{
+                background-image: url(https://66.media.tumblr.com/bcbfa7faed1e55e4054f85b2e0551f62/tumblr_inline_n2590f6RZx1qhwjx8.gif);
+            }
+    h4 {
+            color: lightyellow;
+            background-color:gray;
+            padding: 15px;
+            margin-top: 1%;
+        }
+    </style>
+
+   <nav class="navbar navbar-expand-lg navbar-darger bg-pink">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="home.php">HOME</a>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="sale.php">SALE 12.12</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="transaksi.php">TRANSAKSI</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">ABOUT</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
+        <center><br><br><br><br><br><br><br>
+        <font color="red"><font face="coper black"><font size="20">WELCOME</font></font></font><br>
+        <H1><font color="red"><font face="coper black">PALEMBANG SQUARE</font></font></H1><br>
+        </center>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+
+    
+  </body>
+</html>
